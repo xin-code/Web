c31bb9a0cd9d155d6c2b45d31f797efdab9574de
大宝 <975203723@qq.com>
2020-09-05 11:26:28 +0800

    ES6模块化

---
+++ 18-前端模块化/1-ES6模块化/03-按需导出.js
@@ -0,0 +1,23 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+function dd() {
+    console.log('hanshudd');
+}
+
+export default {
+    a,
+    b,
+    c,
+    dd
+}
+
+
+export let s1 = '111';
+export let s2 = '222';
+export let s3 = '333';
+
+export function say() {
+    console.log('hello');
+}
---
+++ 18-前端模块化/1-ES6模块化/04-按需导入.js
@@ -0,0 +1,7 @@
+import a1, { s1, s2, s3, say } from './03-按需导出.js';
+
+console.log(a1);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(say);
