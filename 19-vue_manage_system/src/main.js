f3656b573ef4917ca93af2ade24a3ce51651f2c3
大宝 <975203723@qq.com>
2020-09-20 23:05:58 +0800

    进度条

--- 19-vue_manage_system/src/main.js
+++ 19-vue_manage_system/src/main.js
@@ -12,13 +12,22 @@ import VueQuillEditor from 'vue-quill-editor'
 // 导入富文本对应的样式
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
-import 'quill/dist/quill.bubble.css' // for bubble theme
+import 'quill/dist/quill.bubble.css'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css' // for bubble theme
+
 // 默认地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 // 拦截器
 axios.interceptors.request.use((config) => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use((config) => {
+  NProgress.done()
   return config
 })
 
