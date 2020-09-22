489de9727e63ded85b5f612056b6e73167caf2ec
大宝 <975203723@qq.com>
2020-09-23 00:53:50 +0800

    Vuex

--- 19-vue_manage_system/src/components/report/reports.vue
+++ 19-vue_manage_system/src/components/report/reports.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- 面包屑导航 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>数据统计</el-breadcrumb-item>
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </el-breadcrumb>
